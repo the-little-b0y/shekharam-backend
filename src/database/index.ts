522125b42f3b5369fb3837eece37ed495ff36246
the-little-b0y <ritwickmohan97@gmail.com>
2022-11-04 22:48:50 +0530

    refactor: Update envs, scripts

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 
 /** Establishing DB Connection */
 export const establishConnection = () => {
-    const mongoURI: string = process.env.MONGO_URI || `mongodb://localhost:27017/shekharam_db`
+    const mongoURI: string = process.env.DATABASE_URL || `mongodb://localhost:27017/shekharam_db`
     connect(mongoURI).then(() => {
         console.info('Database connection Established:', mongoURI);
     }).catch((err) => {
